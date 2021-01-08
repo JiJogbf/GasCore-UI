50b425d725da5bed3b2a095e656375fb416cc3c6
JiJogbf <alex.gas1995@ya.ru>
2021-01-08 20:01:07 +0300

    Chainging from FileLogger to ConsoleLogger in tests of module

--- src/cpp/test/test.cpp
+++ src/cpp/test/test.cpp
@@ -1,13 +1,13 @@
 #include <gas\test\Tester.hpp>
 
 #include <gas\test\Logger.hpp>
-#include <gas\test\FileLogger.hpp>
+#include <gas\test\ConsoleLogger.hpp>
 
 #include "UITypesTestCase.hpp"
 #include "ViewsTestCase.hpp"
 
 int main(){
-    gas::test::Logger* logger = new gas::test::FileLogger("result.log");
+    gas::test::Logger* logger = new gas::test::ConsoleLogger();
     gas::test::Tester tester(logger);
     tester.add(new gas::test::UITypesTestCase(logger));
     tester.add(new gas::test::ViewsTestCase(logger));
